50343960dd250ab95b590a7a338a70b97625df67
JMontney <128848341+JMontney@users.noreply.github.com>
2023-12-07 22:38:33 -0600

    remove safe_demo

--- experiments/py/demo.py
+++ experiments/py/demo.py
@@ -12,68 +12,6 @@ from util.generate import generate_fast
 from util.globals import *
 
 
-def safe_demo_model_editing(
-model: AutoModelForCausalLM,
-    tok: AutoTokenizer,
-    requests: List[Dict],
-    generation_prompts: List[str],
-    alg_name: str = "ROME",
-) -> Tuple[AutoModelForCausalLM, Dict[str, torch.Tensor]]:
-    """
-    Applies the selected model editing algorithm. Generates text both before and after
-    for comparison of model behavior. Returns the updated model and the original values of
-    weights that were changed.
-    """
-
-    nethook.set_requires_grad(True, model)
-
-    RewritingParamsClass, apply_method, hparams_prefix, hparams_suffix = load_alg(
-        alg_name
-    )
-    params_name = (
-        HPARAMS_DIR
-        / hparams_prefix
-        / f"{model.config._name_or_path.replace('/', '_')}{hparams_suffix}.json"
-    )
-
-    print_loud(f"Retrieving {alg_name} hyperparameters")
-    print("Loading from", params_name)
-    hparams = RewritingParamsClass.from_json(params_name)
-    print(hparams)
-
-    print_loud("Generating pre-update text")
-    pre_update_text = generate_fast(model, tok, generation_prompts[0:3], max_out_len=100)
-    print(pre_update_text)
-
-    print_loud(f"Applying {alg_name} to model")
-    model_new, orig_weights = apply_method(
-        model, tok, requests, hparams, return_orig_weights=True
-    )
-
-    print_loud("Generating post-update text")
-
-    post_update_text = generate_fast(
-        model_new, tok, generation_prompts[0:3], max_out_len=100
-    )
-    print(post_update_text)
-
-    print_loud("Summarizing differences")
-    for i, (prompt, pre, post) in enumerate(
-        zip(generation_prompts[0:3], pre_update_text, post_update_text)
-    ):
-        if i > 0:
-            print("".join(["-" for _ in range(10)]))
-
-        prompt_str = "[Prompt]:"
-        pre_str = f"[Pre-{alg_name}]:"
-        post_str = f"[Post-{alg_name}]:"
-        pad_to = 1 + max(len(prompt_str), len(pre_str), len(post_str))
-
-        for s, t in zip([prompt_str, post_str, pre_str], [prompt, post, pre]):
-            print(s.ljust(pad_to), t)
-
-    return model_new, orig_weights
-
 def demo_model_editing(
     model: AutoModelForCausalLM,
     tok: AutoTokenizer,
